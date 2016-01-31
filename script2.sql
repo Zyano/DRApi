0a7a77599a921a580f8316afd87a13242f82786f
Stefan Weibel <stefan.weibel@gmail.com>
2016-02-01 00:29:34 +0100

    part sql upadte

--- script2.sql
+++ script2.sql
@@ -3,8 +3,38 @@ CREATE TABLE AssetStore(
 	title nvarchar(250),
 	barUrl char(63),
 	kind nvarchar(20),
-	fileHash nvarchar(),
-
+	fileHash nvarchar(60),
+	quality char(10)
 );
 
-CREATE TABLE 
+CREATE TABLE Asset(
+	id int identity primary key,
+	description nvarchar(500),
+	title nvarchar(150),
+	airDate datetime,
+	logged datetime,
+
+)
+
+
+CREATE TABLE DownloadLog(
+	id bigint identity primary key,
+	url nvarchar(500),
+	stamp datetime,
+	
+);
+
+
+CREATE TABLE BannedUsers(
+	userId int foreign key references(aspnetUserId),
+	startDate datetime,
+	endDate datetime,
+	ip nvarchar(45), -- IPV6 support
+	reason nvarchar(200),
+);
+
+CREATE TABLE UserFavorites(
+	userId int foreign key references(aspnetUserId),
+
+
+)
