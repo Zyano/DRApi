ed28f2f58757c2f84be659c62a76e497a9172952
Stefan Weibel <stefan.weibel@gmail.com>
2016-05-28 19:54:10 +0200

    SLN updates and some .idea workspace related items.

--- src/DRApiDemo/Program.cs
+++ src/DRApiDemo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using DrApi;
@@ -7,41 +8,42 @@ using DRApi.Bar;
 using DRApi.Requester;
 
 namespace DRApiDemo {
-    public class Program {
-        public static void Main(string[] args) {
-            Program p = new Program();
-            bool run=true;
-            while(run) {
-                Console.WriteLine();
-                Console.WriteLine("New search term");
-                string s=Console.ReadLine();
-                if(s == "exit") {
-                    run=false;
-                } else {
-                    p.GetRes(s).GetAwaiter().GetResult();
-                }
-            }
-            Console.ReadLine();
+  public class Program {
+    public static void Main(string[] args) {
+      Program p=new Program();
+      bool run=true;
+      while(run) {
+        Console.WriteLine();
+        Console.WriteLine("New search term");
+        string s=Console.ReadLine();
+        if(s == "exit") {
+          run=false;
+        } else {
+          p.GetRes(s).GetAwaiter().GetResult();
         }
-
-        public async Task GetRes(string search) {
-            DrRequester dr = new DrRequester();
-            var list = await dr.SearchProgramcardsWithPublicAssetAsync(search);
-
-            foreach(var el in list.Items) {
-                Console.WriteLine(el.SeriesTitle + " - " + el.Title);
-                string uri = el.PrimaryAsset.Uri;
-                Console.WriteLine(uri);
-                var split = uri.Split('/');
-                string barId = split[split.Length - 1];
-                BarResponse br = await dr.BarAsync(barId);
-                Console.WriteLine(br.Links.First(l => l.Target == "HLS").Uri);
-
-
-                string urn = el.Urn;
-                var p = await dr.GetProgramcard(urn);
-                Console.WriteLine(p.Hostname);
-            }
-        }
+      }
+      Console.ReadLine();
     }
+
+    public async Task GetRes(string search) {
+      DrRequester dr=new DrRequester();
+      var list=await dr.ListQuickSearchAsync(search);
+      if(list != null) {
+        if(list.Programs != null) {
+          foreach(var el in list.Programs) {
+            Console.WriteLine(el.SeriesTitle+" - "+el.ProgramcardTitle);
+            var pcard=dr.GetProgramcard(el.ProgramcardSlug).Result;
+            string uri=pcard.PrimaryAsset.Uri;
+            Console.WriteLine(uri);
+            var split=uri.Split('/');
+            string barId=split[split.Length-1];
+            BarResponse br=await dr.BarAsync(barId);
+            Console.WriteLine(br.Links.First(l => l.Target == "HLS").Uri);
+
+            Console.WriteLine(pcard.Hostname);
+          }
+        }
+      }
+    }
+  }
 }
